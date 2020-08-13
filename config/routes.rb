c149b0de253508e823dd6227524625a29dbe0fba
paul sipasseuth <paulsipasseuth@gmail.com>
2020-08-13 16:00:29 +0100

    RAILS frontend - Mister Cocktail - Controller and Models completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "cocktails", to: "cocktails#index"
+
+
+  get "cocktails/new", to: "cocktails#new", as: "new_cocktail"
+  post "cocktails",to: "cocktails#create"
+
+  get "cocktails/:id", to: "cocktails#show", as: "cocktail"
+  # For doses
+
+  # get "cocktails/42/doses/new"
+  # post "cocktails/42/doses"
+  resources :cocktails do
+    resources :doses, only: [ :new, :create ]
+  end
+   # delete "doses/25"
+  resources :doses, only: [ :destroy ]
+
 end
